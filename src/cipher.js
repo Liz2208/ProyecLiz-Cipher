fe4756558494cd98f06f5acb5fc685a96a1dd75a
Liz2208 <108889788+Liz2208@users.noreply.github.com>
2022-07-28 12:13:49 -0500

    Update cipher.js
    falta poco

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,34 @@
-var caesar = caesar || (function() {
+var cipher = cipher|| (function() {
   var doStaff = function (txt, desp, action) {
-    var replace = (function() {
+    var replace = (function() { //llamando funciones
       var abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-      var l = abc.length;
-      return function(c) {
-        var i = abc.indexOf(c.toLowerCase());
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; //aqui llamo las letraspara reemplazar
+      var l = abc.length; //asignando la variable, length es la longitud de la palabra
+      return function(c) { //nombramos esta funcion como  c para llamarla mas abajo
+        var i = abc.indexOf(c.toLowerCase()); //devuelve en mayusculas el valor de la cadena
         if (i != -1) {
           var pos = i;
           if (action) {
-            // forward
             pos += desp;
             pos -= (pos >= l)?l:0;
           } else {
             // backward
             pos -= desp;
-            pos += (pos < 0)?l:0;
+            pos += (pos < 0)?l:0; //condicion de posiciones si esta vacia o con texto
           }
-          return abc[pos];
+          return abc[pos];//aqui llamamos la variable aabc que son las letras a reemplazar
         }
-        return c;
+        return c; //devuelve el valor de la funcion c
       };
     })();
-    var re = (/([a-z])/ig);
-    return String(txt).replace(re, function (match) {
+    var re = (/([a-z])/ig); //formula para reemplazar
+    return String(txt).replace(re, function (match) { //metodo replace
       return replace(match);
     });
   };
 
   return {
-      encode: function(txt, desp) {
+      encode: function(txt, desp) { //reemplaza todos los caracteres excepto los reservados
       return doStaff(txt, desp, true);
     },
       decode: function(txt, desp) {
@@ -38,11 +37,16 @@ var caesar = caesar || (function() {
   };
 })();
 
-function codificar()
+
+function encode() //imprimimos el resultado
 {
-  document.getElementById("resultado").innerHTML=caesar.encode(document.getElementById("cadena").value, 3);
+  document.getElementById("resultado").innerHTML=cipher.encode(document.getElementById("cadena").value, document.getElementById("offset").value);
+  console.log("Este es el espacio a mover", offset.value);
+  console.log("Este es el texto", cadena.value);
+  console.log("Este es el resultado", resultado.value);
+
 }
-function decodificar()
+function decode()
 {
-  document.getElementById("resultado").innerHTML=caesar.decode(document.getElementById("cadena").value, 0);
+  document.getElementById("resultado2").innerHTML=cipher.decode(document.getElementById("cadena").value, 0);
 }
