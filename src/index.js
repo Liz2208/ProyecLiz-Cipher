6d921f65d8570344b4e26aa23447959be23a65b4
Liz2208 <108889788+Liz2208@users.noreply.github.com>
2022-07-30 01:23:24 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import cipher from './cipher.js';
+let userKey = document.getElementById('userOffset');
+let text = document.getElementById('textArea');
+let btn = document.getElementById('submitButton');
+let btn2 = document.getElementById('decipherButton');
+let printResult = document.getElementById('resultado');
+let resultEncrypt = [];
+let resultDecrypt = [];
 
-console.log(cipher);
+btn.addEventListener('click', () => {
+  let textValue = text.value;
+  let userKeyValue = (parseInt(userKey.value));
+  resultEncrypt.toString;
+  cipher.encode(textValue, userKeyValue);
+  printResult.innerHTML = resultEncrypt.join('');
+  console.log("Este es el texto a cifrar", text.value)
+  console.log("Estos son los espacios a mover", userKey.value)
+  });
+
+btn2.addEventListener('click', () => {
+  let userKeyValue = parseInt(userKey.value);
+  resultEncrypt.toString;
+  cipher.decode(resultEncrypt, userKeyValue);
+  printResult.innerHTML = resultDecrypt.join('');
+  console.log("Este es el texto a cifrar", text.value)
+  console.log("Estos son los espacios a mover", userKey.value)
+});
