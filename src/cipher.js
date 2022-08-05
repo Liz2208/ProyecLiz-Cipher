66481f0d6a94e1595168cffb9002836d1a76f357
Liz2208 <108889788+Liz2208@users.noreply.github.com>
2022-08-05 14:41:32 -0500

    Update cipher.js
    ultima version funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,35 @@
 var cipher = {
   encode: (text, key) => {
-    let asciiToLetter;
+
+    let asciiToLetter = '';
     let resultEncrypt = [];
-    var resultEncryptstring = '';
+
     for (let i = 0; i < text.length; i++) {
       let ascii = text.charCodeAt(i);
       if (ascii >= 65 && ascii <= 90) {
-        // Formula para letras mayúsculas
+
         let formula = ((ascii - 65 + key) % 26 + 65);
         resultEncrypt[i] = formula;
-        console.log(formula);
+
       } else if (ascii >= 97 && ascii <= 122) {
-        // Formula para letras minúsculas
         let formulaDos = ((ascii - 97 + key) % 26 + 97);
         resultEncrypt[i] = formulaDos;
+
       } else {
         let formulaAscii = ascii;
         resultEncrypt[i] = formulaAscii;
       }
     }
     for (let j = 0; j < resultEncrypt.length; j++) {
-      asciiToLetter = String.fromCharCode(resultEncrypt[j]);
-      resultEncrypt[j] = asciiToLetter;
-      resultEncryptstring += asciiToLetter;
-
+      asciiToLetter += String.fromCharCode(resultEncrypt[j]);
     }
-    return resultEncryptstring;
+   
+    return asciiToLetter;
 
   },
 
   decode: (resultEncrypt, key) => {
+   
     let resultDecrypt = [];
     for (let k = 0; k < resultEncrypt.length; k++) {
       let asciiDos = resultEncrypt[k].charCodeAt();
@@ -47,6 +47,7 @@ var cipher = {
     for (let l = 0; l < resultDecrypt.length; l++) {
       let asciiToLetterTwo = String.fromCharCode(resultDecrypt[l]);
       resultDecrypt[l] = asciiToLetterTwo;
+      console.log(resultDecrypt);
     }
     return resultDecrypt;
   }
